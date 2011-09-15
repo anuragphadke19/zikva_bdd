604273d29192beabf4e76d42447ea90c2c7f5e5c
Anurag Phadke <anuragphadke19@gmail.com>
2011-09-15 15:33:25 +0530

    Initial COmmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZikvaBdd::Application.config.secret_token = 'a534bcc9301dc726104f0c12f1a0060be532903d638acb4dcac3e7a2bf0e0131cdc240caf9b8b3279b02657fe8467df421cfdee9bd186c793d10fbd754663827'
